c0dde0617ebf33114a316e04e5d6905b39c64584
Tom-Lewis-Coder <tomlewis139@gmail.com>
2022-02-11 13:52:21 +0000

    update cc and layout

--- src/components/ReviewItem.js
+++ src/components/ReviewItem.js
@@ -5,18 +5,18 @@ import { v4 as uuidv4 } from 'uuid'
 const ReviewItem = ({ review, handleDelete, handleEdit }) => {
     return (
         <div className={(review.rating/20) <= 2 ? 'reviewItemBad' : (review.rating/20) === 3 ? 'reviewItemMedium' : 'reviewItemGood'}> 
-            <h2 className='pizzaPlace'>{review.pizzaplace}</h2>
-            <h3 className='date'>{review.date.toDateString()}</h3>
-            <h3 className='custName'>{review.custName}</h3>
-            <h3 className='review'>{review.review}</h3>
-            <h3 className='star'>
+            <div id='pizzaPlaceItem'>{review.pizzaplace}</div>
+            <div id='starItem'>
                 {[...Array(review.rating/20).keys()].map(el => el = <FaStar key={uuidv4()} />)}
-            </h3>
-            <div className='buttons'>
-                <button onClick={() => handleDelete(review.id)} >
+            </div>
+            <div id='reviewItem'>{review.review}</div>
+            <div id='custNameItem'>{review.custName}</div>
+            <div id='dateItem'>{review.date.toDateString()}</div>
+            <div className='buttonItem'>
+                <button id='buttonDelete' onClick={() => handleDelete(review.id)} >
                     <FaTrash />
                 </button>
-                <button onClick={() => handleEdit(review.id)} >
+                <button id='buttonEdit' onClick={() => handleEdit(review.id)} >
                     <FaEdit />
                 </button>
             </div>
